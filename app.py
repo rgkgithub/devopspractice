e97d43fe92e3492d139392a3fe55fb518cdb675b
rgkgithub <raghava.vkumar@gmail.com>
2019-12-11 19:30:24 +0530

    v2

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ year = init(input("Please enter the year you are born:"))
 print(year)
 currentage = 2019-year
 print("Hello " +name)
-print("you are%s old.", current age)
+print("you are %s old.",current age)
