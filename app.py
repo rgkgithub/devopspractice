94688a0dde80de41065fd41d73ed5c8c9172117f
rgkgithub <raghava.vkumar@gmail.com>
2019-12-11 18:54:55 +0530

    added code for age caluculation

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 #caluculate persons age based on year and birth
+name = str(input("Please enter your name:"))
 year = init(input("Please enter the year you are born:"))
+print(year)
+currentage = 2019-year
+print("Hello " +name)
+print("you are%s old.", current age)
